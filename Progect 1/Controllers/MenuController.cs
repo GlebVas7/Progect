2f29eee6a39f63bcd1c37b68bd55226f5bc1eea5
Полина Паничева <pollypanicheva@gmail.com>
2022-05-11 15:09:38 +0300

    back 3.7

--- Progect 1/Controllers/MenuController.cs
+++ Progect 1/Controllers/MenuController.cs
@@ -27,6 +27,6 @@ namespace Progect_1.Controllers
 
         [HttpDelete] //не уверена,что удаление правильно реализовано(в тех контроллерах,где оно есть)
         [Route("menu/{id}")]
-        public Task Delete([FromBody] Guid id) => _manager.DeleteData(id);
+        public Task Delete([FromBody] int id) => _manager.DeleteData(id);
     }
 }
