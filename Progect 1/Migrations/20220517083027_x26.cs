a0301bb2c2b77218bed83dc4e53fa9e9724b901a
Полина Паничева <pollypanicheva@gmail.com>
2022-05-17 11:53:18 +0300

    master 8.1

---
+++ Progect 1/Migrations/20220517083027_x26.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Progect_1.Migrations
+{
+    public partial class x26 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceOfOrder",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PriceOfOrder",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
