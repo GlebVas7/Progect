7616cf645a9f95d4b4832a3711a0accd74a68d4a
IraKirillina <102527668+IraKirillina@users.noreply.github.com>
2022-05-22 16:24:49 +0300

    master10

---
+++ Progect 1/Migrations/20220515125032_x20.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Progect_1.Migrations
+{
+    public partial class x20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.AddColumn<string>(
+                name: "Adress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Orders");
+
+          
+        }
+    }
+}
